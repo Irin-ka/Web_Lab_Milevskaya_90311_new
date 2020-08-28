80204302d80228e8475108bd5ea730750c01d2fa
Ирина Ирина <irin-ka@inbox.ru>
2020-08-28 16:03:41 +0300

    Добавьте файлы проекта.

---
+++ Web_Lab_Milevskaya_90311/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_Lab_Milevskaya_90311.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
